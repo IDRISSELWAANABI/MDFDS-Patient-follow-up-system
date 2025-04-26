b5af89d48c064dc8b10b508486db38a8c28fe7d3
balk <technobadr2003@gmail.com>
2025-04-26 21:37:16 +0100

    Implemented the web_search_formatter

--- src/web_search_formatter/web_search_formatter.py
+++ src/web_search_formatter/web_search_formatter.py
@@ -1,14 +1,14 @@
 import os, sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-from src.llm.query_llm import query_llm
+from src.llm.query_llm import QueryLLM
 from utils.read_config_file import read_config_file
 
 class WebSearchFormatter:
     BASE_PROMPT_FILE = "./src/web_search_formatter/prompts/parse_disease_prompt.txt"
-    CONFIG_FILE = "./config/ollama_config.yml"
+    CONFIG_FILE = "ollama_config.yml"
 
-    def parse_disease(self, text: str) -> dict:
+    def parse_disease(self, text: str) -> str:
         with open(WebSearchFormatter.BASE_PROMPT_FILE, 'r') as file:
             base_prompt = file.read()
         
@@ -18,11 +18,30 @@ class WebSearchFormatter:
             raise ValueError("Model and temperature must be specified in the configuration file.")
 
         prompt = base_prompt.format(text)
-        response = query_llm(
-            prompt=prompt,
+
+        llm = QueryLLM(
             model=model,
             temperature=temperature
+        )
+        response = llm.query(
+            prompt=prompt
         )
         if response is None:
             raise ValueError("LLM returned no response.")
         return response
+    
+if __name__ == "__main__":
+    from src.text_extraction.pdf_to_text_classic import PDFToText
+
+    pdf_file = "/home/balk/Downloads/gen_med_rep_sample.pdf"
+
+    parser = PDFToText(
+        pdf_file=pdf_file
+    )
+    text = parser.extract_text()
+
+    formatter = WebSearchFormatter()
+    result = formatter.parse_disease(text)
+
+    print(result)
+    print(type(result))
